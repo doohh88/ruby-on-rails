eb50ec802955f6c2390fd91d565f48f6a158ad11
doohh88 <doohh88@gmail.com>
2016-03-29 06:00:47 +0000

    first commit

---
+++ timeline/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timeline::Application.config.secret_key_base = '7a65c2bd98d7f8742733f00ab57dd126c7a4733edf7d5e85aa07368896c3ee8f45d7a8da1a5d7fa2b21e85b662416add7c6bb0242c69908ca7c8ce4a1771bcf6'
