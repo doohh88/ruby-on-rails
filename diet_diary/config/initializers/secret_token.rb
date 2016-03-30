e5c5cea99cdc592d36019870d36affe76bfb5c78
doohh88 <doohh88@gmail.com>
2016-03-30 02:20:41 +0000

    diet_diary 1st

---
+++ diet_diary/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DietDiary::Application.config.secret_key_base = 'ccc3a3ad474de3b700d73cb395eed5082fabe22a9e6937eaeaedcd3232de799afad542382bc95424286120b57142e9edfbb7e12f4dcb35d4d57edf246d6f6b25'
