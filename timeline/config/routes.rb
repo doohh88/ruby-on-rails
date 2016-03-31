2eed66e863a19f8b6912e94222be5b98a5baf0fd
doohh88 <doohh88@gmail.com>
2016-03-31 07:33:37 +0000

    timeline final

--- timeline/config/routes.rb
+++ timeline/config/routes.rb
@@ -1,4 +1,13 @@
 Timeline::Application.routes.draw do
+	get "user/signup"
+	post "user/signup_complete"
+	
+	get "user/login"
+	post "user/login_complete"
+  
+	get "user/logout"
+
+	get "user/signup"
   get 'wall/write' => 'wall#write'
 	post "wall/write_complete"
   get 'wall/posts' => 'wall#posts'
